e1f64560af0987f12ea01eeec2200816206f4a73
george redinger <george@georgeredinger.com>
2012-04-05 21:24:54 -0700

    basic

--- test_spec.rb
+++ test_spec.rb
@@ -6,7 +6,10 @@ require './spec'
 class TestDescribe < Test::Unit::TestCase
 	def test_that_it_can_pass
 		describe 'some thing' do
+      it 'has some property' do
 
+
+			end
 
 		end
 	end
@@ -21,3 +24,14 @@ class TestDescribe < Test::Unit::TestCase
 	end
 end
 
+class TestAssertion < Test::Unit::TestCase
+	def test_that_it_can_pass
+		2.should == 2
+	end
+
+	def test_that_it_can_fail
+		assert_raise(AssertionError) do
+		  1.should == 2
+		end
+	end
+end
